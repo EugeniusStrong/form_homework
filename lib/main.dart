eb59e5b5bc0672eb263024c2141a38bcc2fee255
EugeniusStrong <EugeniusStrong@gmail.com>
2023-06-15 13:27:56 +0500

    Final edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:form_homework/pages/register_form_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,9 +8,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      title: 'Register Form HOMEWORK',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const RegisterFormPage(),
+    );
   }
 }
